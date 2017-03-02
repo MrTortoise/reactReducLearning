09295a95aaa251e8cf8be29631382ccf90a982d9
John Nicholas <themrtortoise@gmail.com>
2017-03-02 22:46:23 +0000

    installing packages

---
+++ npminstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+npm install --save \
+    webpack \
+    babel-loader \
+    babel-core \
+    babel-plugin-syntax-jsx \
+    babel-preset-react \
+    babel-preset-es2015 \
+    babel-preset-stage-0
---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir react-intro && cd react-intro
+
+npm init -y
+mkdir src
+mkdir src/components
+mkdir src/store
+mkdir -p dist/js
+mkdir server
+touch src/main.js
+touch server/index.js
